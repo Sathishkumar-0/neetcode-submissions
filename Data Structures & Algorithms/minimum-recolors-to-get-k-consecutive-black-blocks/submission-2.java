49039fafec8e91664369b6f24e0eaffa2b142203
SathishkumarM <128458171+Sathishkumar-0@users.noreply.github.com>
2026-09-12 08:49:14 +0530

    Add: minimum-recolors-to-get-k-consecutive-black-blocks - submission-2

---
+++ Data Structures & Algorithms/minimum-recolors-to-get-k-consecutive-black-blocks/submission-2.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minimumRecolors(String blocks, int k) {
+        int count=0;
+        for(int i=0;i<k;i++){
+            if(blocks.charAt(i)=='W'){
+                count++;
+            }
+        }
+        int res=count;
+        for(int i=k;i<blocks.length();i++){
+            if(blocks.charAt(i-k)=='W'){
+                count--;
+            }
+            if(blocks.charAt(i)=='W'){
+                count++;
+            }
+            res=Math.min(res,count);
+        }
+        return res;
+    }
+}
